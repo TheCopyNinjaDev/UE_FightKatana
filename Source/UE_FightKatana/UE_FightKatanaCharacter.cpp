d7a032508cbdcea37b74e237cffe4dc0bd907fe9
TheCopyNinja <thecopyninjadev@gmail.com>
2022-02-25 15:20:06 +0300

    lock on enemy done

--- Source/UE_FightKatana/UE_FightKatanaCharacter.cpp
+++ Source/UE_FightKatana/UE_FightKatanaCharacter.cpp
@@ -7,6 +7,7 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/Controller.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "UObject/PropertyAccessUtil.h"
 
 //////////////////////////////////////////////////////////////////////////
 // AUE_FightKatanaCharacter
@@ -68,9 +69,9 @@ void AUE_FightKatanaCharacter::SetupPlayerInputComponent(class UInputComponent* 
 	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
 	// "turn" handles devices that provide an absolute delta, such as a mouse.
 	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
-	PlayerInputComponent->BindAxis("Turn Right / Left Mouse", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("Turn Right / Left Mouse", this, &AUE_FightKatanaCharacter::TurnAtRate);
 	PlayerInputComponent->BindAxis("Turn Right / Left Gamepad", this, &AUE_FightKatanaCharacter::TurnAtRate);
-	PlayerInputComponent->BindAxis("Look Up / Down Mouse", this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("Look Up / Down Mouse", this, &AUE_FightKatanaCharacter::LookUpAtRate);
 	PlayerInputComponent->BindAxis("Look Up / Down Gamepad", this, &AUE_FightKatanaCharacter::LookUpAtRate);
 
 	// handle touch devices
@@ -79,15 +80,6 @@ void AUE_FightKatanaCharacter::SetupPlayerInputComponent(class UInputComponent* 
 }
 void AUE_FightKatanaCharacter::LockTarget()
 {
-	if(!IsLockedOnEnemy)
-	{
-		GEngine->AddOnScreenDebugMessage(-1, 12.f, FColor::Green, TEXT("Locked"));
-	}
-	else
-	{
-		GEngine->AddOnScreenDebugMessage(-1, 12.f, FColor::Green, TEXT("Unlocked"));
-	}
-
 	IsLockedOnEnemy = !IsLockedOnEnemy;
 }
 
@@ -103,14 +95,20 @@ void AUE_FightKatanaCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVect
 
 void AUE_FightKatanaCharacter::TurnAtRate(float Rate)
 {
-	// calculate delta for this frame from the rate information
-	AddControllerYawInput(Rate * TurnRateGamepad * GetWorld()->GetDeltaSeconds());
+	if(!IsLockedOnEnemy)
+	{
+		// calculate delta for this frame from the rate information
+		AddControllerYawInput(Rate * TurnRateGamepad * GetWorld()->GetDeltaSeconds());
+	}
 }
 
 void AUE_FightKatanaCharacter::LookUpAtRate(float Rate)
 {
-	// calculate delta for this frame from the rate information
-	AddControllerPitchInput(Rate * TurnRateGamepad * GetWorld()->GetDeltaSeconds());
+	if(!IsLockedOnEnemy)
+	{
+		// calculate delta for this frame from the rate information
+		AddControllerPitchInput(Rate * TurnRateGamepad * GetWorld()->GetDeltaSeconds());
+	}
 }
 
 void AUE_FightKatanaCharacter::MoveForward(float Value)
